2d199d6a2e20290f26c239af096d8a201f606ce7
wern <wern780531@hotmail.com>
2023-01-10 22:02:36 +0800

    week3

--- products.js
+++ products.js
@@ -3,7 +3,11 @@ Vue.createApp({
         return {
             path: 'wern',
             products: [],
-            oneProduct: {},
+            nowProduct: {
+                // imagesUrl: [],
+            },
+            isAddPproduct: true,
+
         }
     },
     methods: {
@@ -24,9 +28,72 @@ Vue.createApp({
                 alert(err.response.data.message);
             })
         },
-        getOneProduct(item) {
-            this.oneProduct = item
-        }
+        openModel(status, item) {
+            if (status === 'add') {
+                this.isAddPproduct = true
+                this.nowProduct = {
+
+                }
+                productModal.show();
+            } else if (status === 'del') {
+                this.nowProduct = item
+                delProductModal.show();
+            } else if (status === 'edit') {
+                this.isAddPproduct = false
+                this.nowProduct = { ...item }
+                productModal.show();
+            }
+        },
+        upProductData() {
+            const api = `https://vue3-course-api.hexschool.io/v2/api/${this.path}/admin/product`;
+
+            if (this.isAddPproduct === true) {
+                axios.post(api, { "data": this.nowProduct }).then((res) => {
+                    this.nowProduct = {}
+                    productModal.hide();
+                    this.getProductsData();
+                })
+            } else if (this.isAddPproduct === false) {
+                axios.put(`${api}/${this.nowProduct.id}`, { "data": this.nowProduct }).then((res) => {
+                    this.nowProduct = {}
+                    productModal.hide();
+                    this.getProductsData();
+                })
+            }
+
+        },
+        delProduct() {
+            const api = `https://vue3-course-api.hexschool.io/v2/api/${this.path}/admin/product/${this.nowProduct.id}`;
+            axios.delete(api).then((res) => {
+                this.nowProduct = {}
+                this.getProductsData();
+                delProductModal.hide();
+                alert(res.data.message)
+            })
+        },
+        delImagesUrl() {
+            // 刪除陣列最後一項
+            this.nowProduct.imagesUrl.pop();
+        },
+        addMainImg() {
+            this.nowProduct.imagesUrl = [];
+            this.nowProduct.imagesUrl.push('');
+        },
+        addImg() {
+            this.nowProduct.imagesUrl.push('')
+        },
+        uploadImg(e) {
+            const api = `https://vue3-course-api.hexschool.io/v2/api/${this.path}/admin/upload`;
+            const file = e.target.files[0];
+            const formData = new FormData();
+            formData.append('file-to-upload', file);
+            axios.post(api, formData)
+                .then((res) => {
+                    console.log(res.data.imageUrl);
+                })
+
+        },
+
     },
     mounted() {
         // 取出 Token(google取得)
@@ -34,5 +101,13 @@ Vue.createApp({
         // 將token放入headers,只需發送一次(google取得)
         axios.defaults.headers.common.Authorization = token;
         this.checkadmin()
+
+        productModal = new bootstrap.Modal(document.getElementById('productModal'), {
+            keyboard: false
+        });
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+            keyboard: false
+        });
     },
 }).mount('#app')
+
